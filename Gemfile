e418c00cdb64490ddbbd7cb4e8d247d1ce966471
Maciej Skierkowski <maciej@skierkowski.com>
2015-04-24 11:27:06 -0700

    Updating gems

--- Gemfile
+++ Gemfile
@@ -6,18 +6,18 @@ gem 'sinatra-contrib'
 gem 'haml', '~> 4.0.6'
 gem 'activemodel', '~> 4.2.1'
 
-gem 'grape', '~> 0.9.0'
+gem 'grape', '~> 0.11.0'
 
-gem 'foreman', '~> 0.77.0'
+gem 'foreman', '~> 0.78.0'
 
-gem 'sidekiq', '~> 3.0.0'
+gem 'sidekiq', '~> 3.3.4'
 
 group :test do
-  gem 'rspec', '~> 3.1.0'
+  gem 'rspec', '~> 3.2.0'
   gem 'rake', '~> 10.4.2'
   gem 'codeclimate-test-reporter', '~> 0.4.1'
   gem 'guard', '~> 2.12.4'
   gem 'guard-rspec', '~> 4.5.0'
   gem 'mock_redis', '~> 0.14.0'
-  gem 'rspec-sidekiq', '~> 1.0.0'
+  gem 'rspec-sidekiq', '~> 2.0.0'
 end
--- app/api.rb
+++ app/api.rb
@@ -18,11 +18,11 @@ module Ephemeral
       params do
         requires :image, type: String, desc: "Docker Image ID"
         requires :commands, type: Array[String], desc: 'List of commands to execute'
-        optional :hooks, type: Array[Hash] do
+        optional :hooks, type: Array do
           requires :event, type: Symbol, values: [:queued, :running, :done, :error], desc: 'Event you want to listen for'
           requires :url, type: String, desc: 'The URL of the web hook.'
         end
-        optional :files, type: Array[Hash] do
+        optional :files, type: Array do
           requires :path, type: String, desc:'The absolute path to the file on the host machine'
           optional :content, type: String, desc: 'The string content of the file'
         end
