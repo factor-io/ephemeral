441493280e1ffabcb9d0c03b81d4448bf7664118
Maciej Skierkowski <maciej@skierkowski.com>
2015-04-27 11:46:01 -0700

    Adding basic framework for providers

---
+++ lib/ephemeral/provider.rb
@@ -0,0 +1,31 @@
+
+
+module Ephemeral
+  class Provider
+
+    def id
+    end
+
+    def commands=(value)
+    end
+
+    def commands
+    end
+
+    def image=(value)
+    end
+
+    def image
+    end
+
+    def upload_file(file, path)
+    end
+
+    def download_file(path)
+    end
+
+    def run
+    end
+
+  end
+end
---
+++ spec/providers/tutum_spec.rb
@@ -0,0 +1,9 @@
+# encoding: UTF-8
+
+require 'spec_helper'
+
+require_relative '../../lib/ephemeral/providers/tutum.rb'
+
+describe Ephemeral::Providers::Tutum do
+  
+end
