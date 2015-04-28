fafb5c229ae922e8b668300ab23a50cb85f37d4f
jasandine <jasandine@gmail.com>
2015-04-28 12:40:59 -0700

    add build class, functionality to create a build, specs passing

---
+++ lib/ephemeral/entities/build.rb
@@ -0,0 +1,13 @@
+require 'grape-entity'
+
+module Ephemeral
+	module Entities
+		class Build < Grape::Entity
+	    expose :id
+	    expose :image
+	    expose :repo
+	    expose :build_type
+	    expose :status
+		end
+	end
+end
