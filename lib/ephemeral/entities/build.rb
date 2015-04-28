f71f545da1f46f1bc48236fa8a897f8d42ac6341
Joey Sandine <jasandine@gmail.com>
2015-04-28 14:22:47 -0700

    Indentation woes

--- lib/ephemeral/entities/build.rb
+++ lib/ephemeral/entities/build.rb
@@ -1,13 +1,13 @@
 require 'grape-entity'
 
 module Ephemeral
-	module Entities
-		class Build < Grape::Entity
-	    expose :id
-	    expose :image
-	    expose :repo
-	    expose :build_type
-	    expose :status
-		end
-	end
+  module Entities
+    class Build < Grape::Entity
+      expose :id
+      expose :image
+      expose :repo
+      expose :build_type
+      expose :status
+    end
+  end
 end
