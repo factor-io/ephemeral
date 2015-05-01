fe81db959ba7470b7e3f5b8f6c50dda451d2229c
Maciej Skierkowski <maciej@skierkowski.com>
2015-05-01 16:07:09 -0700

    Cleaning up broken test

--- spec/api_spec.rb
+++ spec/api_spec.rb
@@ -8,49 +8,14 @@ describe Ephemeral::API do
   def app
     Ephemeral::API
   end
-<<<<<<< HEAD
-  
-
-  request_data = {
-    image: 'ubuntu',
-    commands: ['ls'],
-    files: [
-      {
-        path:'/foo',
-        content: 'echo "Hello World!"'
-      }
-    ]
-  }
-
-  describe 'Jobs' do
-    it 'can create' do
-
-      post '/v1/jobs', request_data
-      
-      response_data = JSON.parse(last_response.body)
-
-      expect(last_response.status).to eq(201), response_data['error']
-      expect(response_data.keys).to include('id')
-      expect(response_data.keys).to include('status')
-
-
-      expect(Ephemeral::Worker).to have_enqueued_job(response_data)
-    end
-  end
-
-=======
->>>>>>> a5a5d294f4ad9760728fea204dbe22bdae48c8b0
-
-  request_build = {
-    image: 'ruby:2.1',
-    repo: 'http://github.com/skierkowski/hello-middleman',
-    build_type: 'middleman'
-  }
-
 
   describe 'Builds' do
     it 'can create' do 
-
+      request_build = {
+        image: 'ruby:2.1',
+        repo: 'http://github.com/skierkowski/hello-middleman',
+        build_type: 'middleman'
+      }
       post '/v1/builds', request_build
 
       response_build = JSON.parse(last_response.body)
