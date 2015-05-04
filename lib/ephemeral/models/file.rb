9faf2718e4724e6ad89fffc2f259af25bb051548
Clem <clemfree@gmail.com>
2015-05-04 14:05:26 -0700

    Add status to file model

--- lib/ephemeral/models/file.rb
+++ lib/ephemeral/models/file.rb
@@ -6,8 +6,9 @@ module Ephemeral
       attr_accessor :id, :file
 
       def initialize(file_settings={})
-        @id = file_settings[:id] || SecureRandom.hex(8)
-        @file = file_settings[:file] || ''
+        @id     = file_settings[:id] || SecureRandom.hex(8)
+        @file   = file_settings[:file] || ''
+        @status = :new
       end
     end
   end
