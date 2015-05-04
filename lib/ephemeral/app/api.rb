0aef8be4a3f5b957d3dc164a8f9fecbce617ecf1
Clem <clemfree@gmail.com>
2015-05-04 16:07:01 -0700

    Add require statements for file

--- lib/ephemeral/app/api.rb
+++ lib/ephemeral/app/api.rb
@@ -4,6 +4,9 @@ require_relative './worker.rb'
 require_relative '../models/build.rb'
 require_relative '../entities/build.rb'
 require_relative './resources/build.rb'
+require_relative '../models/file.rb'
+require_relative '../entities/file.rb'
+require_relative './resources/file.rb'
 
 module Ephemeral
   class API < Grape::API
