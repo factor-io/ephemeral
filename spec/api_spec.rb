7e2ef2ce97a48145b40443964d13d5575fdc3379
Joey Sandine <jasandine@gmail.com>
2015-05-13 09:59:13 -0700

    Update api_spec.rb

--- spec/api_spec.rb
+++ spec/api_spec.rb
@@ -159,19 +159,5 @@ describe Ephemeral::API do
 
       expect(content['status']).to eq('done')
     end
-
-  it 'can perform a build' do
-    build_settings = {
-      'id' =>        "test-#{SecureRandom.hex(4)}",
-      'repo' =>      'https://github.com/skierkowski/hello-middleman/archive/master.zip',
-      'image' =>     'ruby:2.1',
-      'build_type' =>'middleman'
-    }
-      worker = Ephemeral::Worker.new
-      response = worker.perform(build_settings)
-
-      expect(response).to be_a(Hash)
-      expect(response).to have_key('logs')
-    end
   end
 end
