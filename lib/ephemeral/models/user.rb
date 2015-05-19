5cd8c8da41b3c5a1978c96b8870d5c6b108c3996
jasandine <jasandine@gmail.com>
2015-05-19 15:25:47 -0700

    Create users table in DB

--- lib/ephemeral/models/user.rb
+++ lib/ephemeral/models/user.rb
@@ -2,14 +2,7 @@ require 'securerandom'
 
 module Ephemeral
   module Models
-    class User
-      attr_accessor :email, :password, :id
-
-      def initialize(params={})
-        @id          = user_settings[:id] || SecureRandom.hex(8)
-        @email       = user_settings[:email] || ''
-        @password    = user_settings[:password] || ''
-      end
+    class User < Sequel::model
     end
   end
 end
